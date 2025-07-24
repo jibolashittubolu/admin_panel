ec4660224fef138dc0ffbd5666c6895d95db1493
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2025-07-24 20:15:51 +0100

    Update Chart.jsx

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,7 +1,5 @@
 import React, { PureComponent } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-
 import './chart.scss'
 function Chart({data, title, dataKey, grid}) {  
   return (
