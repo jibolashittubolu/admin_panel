35d895e553122f3e478cc8a5efd335dd386c4540
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2025-11-18 00:56:17 +0100

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -4,6 +4,7 @@ const reportWebVitals = onPerfEntry => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
+      
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
