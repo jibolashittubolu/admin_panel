5dd1aef52172184cb86547b3b9838212fe4c1cc5
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2026-04-15 20:49:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 //import 
 import logo from './logo.svg';
 import './App.scss'; 
-
 import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './pages/home/Home';
